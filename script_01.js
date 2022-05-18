b6a610afd138fab5f5edc25f3d3be5757235f8e5
Mariusz PS <103026332+MariuszPS@users.noreply.github.com>
2022-05-18 14:51:33 +0200

    1+2

--- script_01.js
+++ script_01.js
@@ -8,17 +8,49 @@
 // output("------------");
 
 
-
 /***** Objekte 1 Daten/Funktionen *******/
 
+let person =    {
+                        firstName: "Christof",
+                        famillyName: "Psurek",
+                        salary: [120000,160000],
+                        permission: true,
+                        sayHello: 
+                        function (){
+                           return "Hallo, " + this.firstName;
+                        }
+                 };
 
+// output(person);
+// output(person.firstName);
+// output(person.permission);
+
+const txt =     "Ich bin " + person.firstName + " " + 
+                person.famillyName + " und verdiene " +  
+                person.salary[0] + " p.a" +
+                ".";
+
+// output(txt);       
+// output(person.sayHello());           
 
 
 /***** Objekte 2 - Hierarchie *******/
+// Tiefer, tiefer - Irgendwo in der Tiefe gibt es ein Licht // K.Bush
+// Der Baikalsee ist mit 1642 m der tiefste See der Erde. ...
+// deep - deeper - deepest
 
+const baikal = {
+        value:"10m",
+        deep:{
+                deeper:{
+                        deepest:"Das Licht - auf 1642m!"
+                }
+        }
+};
 
-
-
+output(baikal);
+output(baikal.value);
+output(baikal.deep.deeper.deepest);
 
 /** Ausgabe */
 function output(outputData) {
